b82236a654c6cfc5bb641015541c3a6a95d0a87f
Jonathan White <thinkdigitalrepair@gmail.com>
2018-09-15 12:43:57 -0700

    Added all icons from the pack to the example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:groovin_material_icons/groovin_material_icons.dart';
+import 'package:groovin_material_icons_testapp/icon_map.dart';
+import 'package:draggable_scrollbar/draggable_scrollbar.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,26 +29,112 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var scrollController = ScrollController();
+  var fabIcon = GroovinMaterialIcons.flutter;
+  Function refreshFab;
+  var iconList;
+  @override
+  void initState() {
+    iconList = iconMap
+        .map((title, icon) => MapEntry(
+            title,
+            new IconCard(
+              title: title,
+              icon: icon,
+            )))
+        .values
+        .toList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    var crossAxisCount = (MediaQuery.of(context).size.width / 150).ceil();
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(icon: Icon(GroovinMaterialIcons.send_outline), onPressed: (){})
-          ],
+      body: DraggableScrollbar.rrect(
+        backgroundColor: Theme.of(context).primaryColor,
+        controller: scrollController,
+        child: GridView.builder(
+          physics: PageScrollPhysics(),
+          controller: scrollController,
+          padding: EdgeInsets.all(10.0),
+          itemCount: iconMap.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: crossAxisCount),
+          itemBuilder: (BuildContext context, int index) {
+            return iconList[index];
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {},
+        child: Icon(fabIcon),
+      ),
+    );
+  }
+}
 
-        },
-        child: Icon(GroovinMaterialIcons.flutter),
+class IconCard extends StatefulWidget {
+  final String title;
+  final IconData icon;
+
+  const IconCard({
+    Key key,
+    this.title,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  IconCardState createState() {
+    return new IconCardState();
+  }
+}
+
+class IconCardState extends State<IconCard> {
+  double elevation = 0.0;
+  double iconSize = 35.0;
+
+  FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () async {
+        setState(() {
+          //elevation = (elevation == 0.0) ? 5.0 : 0.0;
+          iconSize += 1;
+        });
+        await Future.delayed(Duration(milliseconds: 200));
+        setState(() {
+          // elevation = (elevation == 0.0) ? 5.0 : 0.0;
+          iconSize -= 1;
+        });
+      },
+      child: Card(
+        elevation: elevation,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                widget.icon,
+                size: iconSize,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: fontWeight),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
